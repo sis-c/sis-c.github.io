0eb022e1e720479432de8fcfee19a827c97b09a4
sis-c <35248903+sis-c@users.noreply.github.com>
2019-10-11 16:32:31 -0600

    Update menu.js

--- kian/js/menu.js
+++ kian/js/menu.js
@@ -2,9 +2,9 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
     
     if (document.getElementsByClassName("navigation")[0].classList.contains("responsive")) {
-        document.getElementsByClassName("navigation")[0].innerHTML = "&#9748; Home";
+        document.getElementsByClassName("navigation")[0].children[0].innerHTML = "&#9748; Home";
     }
     else {
-        document.getElementsByClassName("navigation")[0].innerHTML = "&#9730; Menu";
+        document.getElementsByClassName("navigation")[0].children[0].innerHTML = "&#9730; Menu";
     }
 }

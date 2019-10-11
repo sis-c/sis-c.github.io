b4e26604372537670ca1a478d6a7a671b3f8d607
sis-c <35248903+sis-c@users.noreply.github.com>
2019-10-11 16:22:30 -0600

    b

--- kian/js/menu.js
+++ kian/js/menu.js
@@ -1,10 +1,10 @@
 function toggleMenu() {
     var x = document.getElementsByClassName("navigation")[0];
     x.classList.toggle("responsive");
-    if (x.classList.contains("responsive")) {
+    /*if (x.classList.contains("responsive")) {
         x.innerHTML = "&#9748; Home";
     }
     else {
         x.innerHTML = "&#9730; Menu";
-    }
+    }*/
 }

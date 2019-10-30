a03c34087696625555373f6d037baecd7686c737
sis-c <campbellka@byui.edu>
2019-10-30 11:39:11 -0600

    amy

---
+++ lesson7activity/js/apps.js
@@ -0,0 +1,53 @@
+const imagesToLoad = document.querySelectorAll("[data-src]");
+console.log(imagesToLoad)
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+  img.src = src;
+  img.removeAttribute("data-src");
+}
+const imageOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 0px 0px"
+};
+
+const imgObserver = new IntersectionObserver((items, observer) => {
+  items.forEach(item => {
+    if (!item.isIntersecting) {
+      return;
+    }
+    else {
+      preloadImage(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+}, imageOptions);
+
+
+imagesToLoad.forEach(image => {
+  imgObserver.observe(image);
+});
+
+
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {image.removeAttribute('data-src');};
+// };
+
+//   if('IntersectionObserver' in window) {
+//     const imgObserver = new IntersectionObserver((items, observer) => {
+//       items.forEach((item) => {
+//        });
+//     }, imageOptions);
+
+//     imagesToLoad.forEach((img) => {
+//       imgObserver.observe(img);
+//     });
+//   } else {
+//     imagesToLoad.forEach((img) => {
+//       loadImages(img);
+//     });
+//   }
